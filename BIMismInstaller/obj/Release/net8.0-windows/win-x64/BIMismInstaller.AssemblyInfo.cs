df712d6419cb0b058792fe61bb360e416bda0e73
Asanka Dharmarathna <asankadharmarathna78@gmail.com>
2026-01-14 22:37:00 +0530

    Release v2.3.1: UI Fixes (Overlap, Labels, Previews)

--- BIMismInstaller/obj/Release/net8.0-windows/win-x64/BIMismInstaller.AssemblyInfo.cs
+++ BIMismInstaller/obj/Release/net8.0-windows/win-x64/BIMismInstaller.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BIMism_AI_Agent_Installer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9528f6fe4f234af6776ad204d0dbc26fc31dcd8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b814d6e636c13e232bc966de1c2b66c3fffc8f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BIMism_AI_Agent_Installer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BIMism_AI_Agent_Installer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
